6894cf5958e79f6dd965ad8fe9f044f3653bf7e7
pnh0002 <pnietohurtado03@gmail.com>
2025-10-03 18:09:28 +0200

    @Configuration

--- proyecto01/src/main/java/com/proyecto01/proyecto01/controllers/ProductRestController.java
+++ proyecto01/src/main/java/com/proyecto01/proyecto01/controllers/ProductRestController.java
@@ -1,9 +1,11 @@
 package com.proyecto01.proyecto01.controllers;
 
+import com.proyecto01.proyecto01.configurations.ExternalizedConfigurations;
 import com.proyecto01.proyecto01.models.Product;
 import com.proyecto01.proyecto01.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +18,23 @@ import java.util.List;
 public class ProductRestController {
 
     @Autowired
+    @Lazy
     //@Qualifier("JAVA") // Elije la interfaz que vamos a utilizar
     private ProductService repo;
+
+    @Autowired
+    @Lazy
+    private ExternalizedConfigurations config;
 
 
     @GetMapping
     private ResponseEntity<?> getProducts() {
+        System.out.println(config.toString());
+
         List<Product> products = repo.getProducts();
         return ResponseEntity.ok(products);
     }
+
+
 
 }
