80262c0632be9d21618eaa0895d8de6983824bbe
pnh0002 <pnietohurtado03@gmail.com>
2025-10-06 16:47:02 +0200

    Proyecto con API y base de datos

---
+++ crud/src/main/java/com/api/crud/services/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.api.crud.services;
+
+public class UserServiceImpl {
+}
--- proyecto01/src/main/java/com/proyecto01/proyecto01/service/CoustomerServiceImpl.java
+++ proyecto01/src/main/java/com/proyecto01/proyecto01/service/CoustomerServiceImpl.java
@@ -1,4 +1,18 @@
 package com.proyecto01.proyecto01.service;
 
+import com.proyecto01.proyecto01.models.Customer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CoustomerServiceImpl {
+
+    List<Customer> customers = new ArrayList<>(Arrays.asList(
+            new Customer(1, "Pablo" , "pnh0003", "123"),
+            new Customer(2, "Pepe" , "jph0009", "123"),
+            new Customer(3, "Juan" , "pyh0069", "123"),
+            new Customer(4, "Jose" , "ong0002", "123")
+    ));
+
 }
