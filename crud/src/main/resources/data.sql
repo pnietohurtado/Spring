40c5e7897358261c07aaf6cd8cb625854cb07dc6
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-10-10 17:34:05 +0200

    Creamos los schemas de la base de datos

---
+++ crud/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user(id,firstName, lastName, email) VALUES (1, 'Pablo', 'Nieto', 'pnh0002@alu.medac.es');
+INSERT INTO user(id,firstName, lastName, email) VALUES (2, 'Pepe', 'Ortega', 'pnh0002@alu.medac.es');
+INSERT INTO user(id,firstName, lastName, email) VALUES (3, 'Juan', 'Hurtado', 'pnh0002@alu.medac.es');
+INSERT INTO user(id,firstName, lastName, email) VALUES (4, 'Angel', 'Gasol', 'pnh0002@alu.medac.es');
---
+++ crud/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table user(
+
+    id INT NOT NULL PRIMARY KEY,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL
+
+);
