6672cd0667351dfa872f4cd8bd3188d0da2302a0
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-11-23 20:56:56 +0100

    Prueba Chat

---
+++ PruebaChat/src/main/java/com/pruebaChat/PruebaChat/PruebaChatApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaChat.PruebaChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaChatApplication.class, args);
+	}
+
+}
---
+++ PruebaChat/src/test/java/com/pruebaChat/PruebaChat/PruebaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaChat.PruebaChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
