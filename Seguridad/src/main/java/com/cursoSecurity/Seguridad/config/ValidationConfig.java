579c7dc386f0069d9e7c300c26232418c4f01843
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-11-30 12:44:44 +0100

    CorsConfiguration

---
+++ Seguridad/src/main/java/com/cursoSecurity/Seguridad/config/ValidationConfig.java
@@ -0,0 +1,15 @@
+package com.cursoSecurity.Seguridad.config;
+
+import com.cursoSecurity.Seguridad.service.models.validation.UserValidation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ValidationConfig {
+
+    @Bean
+    public UserValidation validation(){
+        return new UserValidation();
+    };
+
+}
