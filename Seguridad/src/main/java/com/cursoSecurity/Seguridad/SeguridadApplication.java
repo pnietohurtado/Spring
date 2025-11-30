9553a0803794c7d4b10f683b2e4c02b26997d656
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-11-30 11:56:05 +0100

    Seguridad v2

---
+++ Seguridad/src/main/java/com/cursoSecurity/Seguridad/SeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.cursoSecurity.Seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadApplication.class, args);
+	}
+
+}
