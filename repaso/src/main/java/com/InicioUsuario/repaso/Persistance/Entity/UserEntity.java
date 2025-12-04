dfe1fd89f1be272582f70c68561073c5d5e6d28b
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-12-04 16:41:49 +0100

    Creación de las Clases dentro de Persistance

---
+++ repaso/src/main/java/com/InicioUsuario/repaso/Persistance/Entity/UserEntity.java
@@ -0,0 +1,89 @@
+package com.InicioUsuario.repaso.Persistance.Entity;
+
+import com.InicioUsuario.repaso.Persistance.Enum.Role;
+import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class UserEntity implements UserDetails {
+
+    // Parámetros de la entidad
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uuid; // Id del personaje
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private Role role;
+
+    // Constructor de la clase
+    public UserEntity(){} // Muy importante tener un constructor vacío
+
+    // Getter y Setter
+
+    public void setUuid (Long id){this.uuid = id; }
+    public void setFirstName(String name) {this.firstName = name; }
+    public void setLastName (String name) {this.lastName = name; }
+    public void setEmail(String email){this.email = email; }
+    public void setPassword(String password){this.password = password; }
+    public void setRole(Role role){this.role = role; }
+    public void setUsername(String username){this.username = username; }
+
+    public Long getUuid() {
+        return uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    //  Métodos implementados desde la interfaz de UserDetails
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+}
