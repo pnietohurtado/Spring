6d9dc1a5daca61f6591aaf79c7e0c91d5acf0ebb
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-12-08 19:08:09 +0100

    Cambios sobre el controller del Usuario para limitar el acceso a los roles más altos

--- repaso/src/main/java/com/InicioUsuario/repaso/Controller/UserController.java
+++ repaso/src/main/java/com/InicioUsuario/repaso/Controller/UserController.java
@@ -4,11 +4,16 @@ import com.InicioUsuario.repaso.Persistance.Entity.UserEntity;
 import com.InicioUsuario.repaso.Service.Impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,8 +27,40 @@ public class UserController {
     @GetMapping("/findAll")
     //@PreAuthorize("hasAuthority('ADMIN')")
     public List<UserEntity> findAllUsers(){
+
+        /*
+        if(hasRole("ROLE_ADMIN")){
+            System.out.println("Hi, you has access");
+            return service.findAllUsers();
+        }else{
+            System.out.println("You dont!");
+        }
+        */
+
         return service.findAllUsers();
     }
 
+
+    private boolean hasRole(String role){
+        SecurityContext context = SecurityContextHolder.getContext();
+
+        if(context == null){
+            return false;
+        }
+
+        Authentication auth = context.getAuthentication();
+
+        if(auth == null){
+            return false;
+        }
+
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        for(GrantedAuthority authority : authorities){
+            if(role.equals(authority.getAuthority())){
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
