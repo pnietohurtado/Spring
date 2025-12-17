78decca706ffc2f57119274c4d903b24271f7bf0
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-12-17 20:07:40 +0100

    Terminados los websockets

--- demo/src/main/java/com/websocket/demo/Controller/ChatController.java
+++ demo/src/main/java/com/websocket/demo/Controller/ChatController.java
@@ -1,7 +1,24 @@
 package com.websocket.demo.Controller;
 
+import com.websocket.demo.Persistance.Models.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages") // Where the message is going to end
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+
+    @GetMapping("/chat")
+    public String chat(){
+        return "chat";
+    }
+
 }
---
+++ demo/src/main/java/com/websocket/demo/Persistance/Models/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.websocket.demo.Persistance.Models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+
+    private Long id;
+    private String sender;
+    private String content;
+
+
+}
