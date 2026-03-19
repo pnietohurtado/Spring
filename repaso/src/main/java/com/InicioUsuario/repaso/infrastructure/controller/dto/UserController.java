f6d292c914bc62c03faeea805b020f85dbf5cc86
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2026-03-19 14:20:33 +0100

    Find all

---
+++ repaso/src/main/java/com/InicioUsuario/repaso/infrastructure/controller/dto/UserController.java
@@ -0,0 +1,12 @@
+package com.InicioUsuario.repaso.infrastructure.controller.dto;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+
+
+}
